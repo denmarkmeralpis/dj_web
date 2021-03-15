183f1743e6c43440829cbf6b36e25065385a4226
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2021-03-15 17:24:18 +0800

    Added env for security

--- app/controllers/dj_web/application_controller.rb
+++ app/controllers/dj_web/application_controller.rb
@@ -1,5 +1,19 @@
 module DjWeb
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    before_action :authenticate_request
+
+    private
+
+    def authenticate_request
+      if ENV['DJ_WEB_USERNAME'].blank? || ENV['DJ_WEB_PASSWORD'].blank?
+        render text: 'Unauthorized request', status: 401
+      else
+        authenticate_or_request_with_http_basic do |user, pass|
+          user == ENV['DJ_WEB_USERNAME'] && pass == ENV['DJ_WEB_PASSWORD']
+        end
+      end
+    end
   end
 end
